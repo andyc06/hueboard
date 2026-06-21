c03f0cc03ae843f6a8e08ffb111f23e97cf98d2d
Andy C <andrew@andrewben.com>
2026-06-20 21:56:29 -0700

    many changes to get reactivity working and add checked prop

--- app/app.vue
+++ app/app.vue
@@ -1,16 +1,47 @@
 <script setup lang="ts">
-const { status, data: lights } = await useFetch("/api/lights");
+import { computed, watch } from "vue";
+import Light from "./components/Light.vue";
+
+const { status, data } = await useFetch("/api/light");
+
+// Create a new deeply reactive array
+const lights = ref([]);
+
+watchEffect(() => {
+  if (!data.value) return;
+
+  // Replace with a fresh reactive array
+  lights.value = data.value.map((light) => ({
+    ...light,
+    name: light.name ?? "",
+    isOn: light.isOn ?? false,
+    checked: false,
+  }));
+});
+
+const checkedItems = computed(() =>
+  (lights.value ?? []).filter((i) => i.checked).map((i) => i.id),
+);
 </script>
+
 <template>
   <h1>hueboard</h1>
-  <h3>light list</h3>
-  <div>
-    <div v-if="status === 'pending'">Loading...</div>
-    <div v-else-if="status === 'error'">Error loading posts</div>
-    <div v-else>
-      <div v-for="light in lights">
-        name: {{ light.name }} | on: {{ light.isOn }}
-      </div>
+  <div v-if="status === 'pending'">Loading lights...</div>
+  <div v-else-if="status === 'error'">Error loading lights</div>
+
+  <div v-else>
+    <h3>lights:</h3>
+    <Light
+      v-for="light in lights"
+      :key="light.id"
+      :id="light.id"
+      :name="light.name"
+      :isOn="light.isOn"
+      v-model="light.checked"
+    />
+    <div>
+      <h3>tray:</h3>
+      {{ checkedItems }}
     </div>
   </div>
 </template>
---
+++ app/components/Light.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const props = defineProps<{
+  id: number;
+  name: string;
+  isOn: boolean;
+}>();
+
+// This replaces BOTH modelValue + update:modelValue
+const checked = defineModel<boolean>();
+</script>
+
+<template>
+  <div>
+    <input type="checkbox" v-model="checked" />
+    name: {{ name }} | on: {{ isOn }}
+  </div>
+</template>
